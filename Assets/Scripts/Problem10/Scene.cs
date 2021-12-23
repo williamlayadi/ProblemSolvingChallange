df61c3bcb4a1a3239701905d60b5ccb4fe562e8b
williamlayadi <layadi.william123@gmail.com>
2021-12-23 20:49:05 +0700

    Problem10

---
+++ Assets/Scripts/Problem10/Scene.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void goToProblem1()
+    {
+        SceneManager.LoadScene("Problem1");
+    }
+    public void goToProblem2()
+    {
+        SceneManager.LoadScene("Problem2");
+    }
+    public void goToProblem3()
+    {
+        SceneManager.LoadScene("Problem3");
+    }
+    public void goToProblem4()
+    {
+        SceneManager.LoadScene("Problem4");
+    }
+    public void goToProblem5()
+    {
+        SceneManager.LoadScene("Problem5");
+    }
+    public void goToProblem6()
+    {
+        SceneManager.LoadScene("Problem6");
+    }
+    public void goToProblem7()
+    {
+        SceneManager.LoadScene("Problem7");
+    }
+    public void goToProblem8()
+    {
+        SceneManager.LoadScene("Problem8");
+    }
+    public void goToProblem9()
+    {
+        SceneManager.LoadScene("Problem9");
+    }
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene("Problem10");
+    }
+}
