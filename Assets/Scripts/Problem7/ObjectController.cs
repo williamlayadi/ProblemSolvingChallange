d766f0e24077e0a2d12363955b69e95cbdc5acb4
williamlayadi <layadi.william123@gmail.com>
2021-12-23 16:56:20 +0700

    Problem7

---
+++ Assets/Scripts/Problem7/ObjectController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public ScoreTextController _ScoreTextController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ScoreTextController = FindObjectOfType<ScoreTextController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _ScoreTextController.addScore();
+        Destroy(gameObject);
+    }
+}
